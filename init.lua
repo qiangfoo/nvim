8c47c8d62d1fd0695572c4d07d06571cc2aa5040
John Fu <jfu@jfu-ltm.internal.salesforce.com>
2022-02-15 10:01:00 -0800

    init project

---
+++ init.lua
@@ -0,0 +1,44 @@
+require('plugins')
+vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.mouse = 'a'
+
+require('nightfox').load("nightfox")
+
+require('nvim-tree').setup {
+    view = {
+        width = 30,
+        auto_resize = true,
+        side = 'right'
+    }
+}
+require('lualine').setup{}
+
+local cmp = require('cmp')
+cmp.setup{
+  snippet = {
+    expand = function (args)
+      vim.fn["vsnip#anonymous"](args.body) 
+    end
+  },
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({ select = true })
+  },
+  sources = cmp.config.sources({
+    { name = 'buffer' }
+  })
+}
+
+-- keymaps
+local keymap = function (mode, from, to)
+  vim.api.nvim_set_keymap(mode, from, to, { noremap = true, silent = true })
+end
+
+keymap('n', '<Leader>fb', "<cmd>lua require('fzf-lua').blines()<CR>")
+keymap('n', '<Leader>ff', "<cmd>lua require('fzf-lua').files()<CR>")
+keymap('n', '<Leader>p', '"+P')
+keymap('v', '<Leader>y', '"+y')
+keymap('n', '<Leader>c', ':BufferClose<CR>')
---
+++ lsp.lua
@@ -0,0 +1,23 @@
+
+--  use 'hrsh7th/cmp-nvim-lsp'
+--  use 'neovim/nvim-lspconfig'
+--local on_attach_handler = function (client, bufnr)
+ -- require 'illuminate'.on_attach(client)
+--end
+
+--local lspconfig = require('lspconfig')
+--local servers = { 'tsserver' }
+
+--local cmp_lsp = require('cmp_nvim_lsp')
+--local capabilities = cmp_lsp.update_capabilities(
+--  vim.lsp.protocol.make_client_capabilities()
+--)
+--for _, server in pairs(servers) do
+--  lspconfig[server].setup {
+--    on_attach = on_attach_handler,
+--    capabilities = capabilities,
+--    flags = {
+--      debounce_text_changes = 150
+--    }
+--  }
+--end
